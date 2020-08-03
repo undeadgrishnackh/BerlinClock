8dd508b402b7ebbdc4f133747ccb6570b1c13ed4
Michele Brissoni <michele.brissoni@ibm.com@Micheles-MacBook-Pro-2.local>
2020-08-03 10:44:32 +0200

    Refactor: class extraction in progress

--- NOTES.md
+++ NOTES.md
@@ -2,35 +2,40 @@
 ✅ - test for highlighting even & odds in the second blinker - check the - line5
 ✅ - improve the extraction from the Berlin Clock string to understand what we're looking into the test.
 ✅ - minutes lights: 2 extracts functions for the minutes, apply the second test pattern
-   --> 1. update the describe 
-   --> 2. reduce the number of duplicated functions
+   ✅ --> 1. update the describe 
+   ✅ --> 2. reduce the number of duplicated functions
 ✅ - hours: refactor to the minute test pattern; add 2 first row test 05:00:00, 20:00:00; update the describes;
 ✅ - remove the duplication in expect*Row functions
 ✅ - seconds: check if the second is described as the top light
 ✅ - E2E: description
-TODO: - the code is messy and too function-oriented. 
- ✅ -----> restart from render5Minutes refactor.
- TODO: restart the refactor rendering the components like: seconds, hours, minutes
-      - extract the render color method as return into the minutes
-      - RESTART & deep REFACTOR:
-        ❓ - cluster hours in **one** render method
-        ❓ - cluster minutes in **one** render method
+✅ - the code is messy and too function-oriented. 
+ ✅ ---> restart from render5Minutes refactor.
+ ✅: restart the refactor rendering the components like: seconds, hours, minutes
+      ✅ - extract the render color method as return into the minutes
+      ✅ - RESTART & deep REFACTOR:
+        ❓--> ❌ - cluster hours in **one** render method
+        ❓--> ❌ - cluster minutes in **one** render method
         ✅ - refactor seconds to call it properly as the domain
 
-TODO:
 ✅ - sec & hours tests have to be enhanced with the DDD desc.
 ✅ - top and second rows are shuffled into the tests
 ✅ - try to create a BerlinClock class that take care to render ✅ sec, ✅ hours, ✅ mins starting from sec.
 
+TODO:  
+ - ParsedTime 
+  ✅ - remove duplication in methods (splitting)
+  TODO: - extract it into a time class with the regexp validator as splitter
+ - Clock
+  ❓- extract every row in a specific class
+  ❓- the clock is a Matryoshka composition of seconds, hours, minutes and every component is itself a row. 
+
 
 
 ## Refactoring of the test suites in a more DDD compliant language:
-
-❓. The "Berlin Clock" is the first public clock in the world that tells the time by means of illuminated, colored fields, for which it entered the Guinness Book of Records upon its installation on 17 June 1975.
-
+✅. The "Berlin Clock" is the first public clock in the world that tells the time by means of illuminated, colored fields, for which it entered the Guinness Book of Records upon its installation on 17 June 1975.
 ✅. The clock is read from the top row to the bottom. 
 ✅. The top row of four red fields denote five full hours each, alongside the second row, also of four red fields, which denote one full hour each, displaying the hour value in 24-hour format.
-TODO:. The third row consists of eleven yellow-and-red fields, which denote five full minutes each (the red ones also denoting 15, 30 and 45 minutes past), and the bottom row has another four yellow fields, which mark one full minute each. 
+✅. The third row consists of eleven yellow-and-red fields, which denote five full minutes each (the red ones also denoting 15, 30 and 45 minutes past), and the bottom row has another four yellow fields, which mark one full minute each. 
 ✅. The round yellow light on top blinks to denote even- (when lit) or odd-numbered (when unlit) seconds.
 
 
--- src/sandbox.js
+++ src/sandbox.js
@@ -1,15 +1,122 @@
-const VALID_TIME_PATTERN = new RegExp('^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$')
+// Function to solve the kata
+function berlinClock(time) {
+  if (isTimeValid(time)) {
+    return new BerlinClock(new ParsedTime(time)).render();
+  }
+  return 'error';
+}
 
+// --------------------------------------------------------------- TIME
+//TODO: use the regexp to validate and split the sec, hou, min...
+const VALID_TIME_PATTERN = new RegExp('^([0-9]|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$')
+const isTimeValid = time => VALID_TIME_PATTERN.test(time);
 
+//TODO: is this a parser only or the class time itself?
 class ParsedTime {
+  
+  constructor(time) {
+      this.parts = time.split(":")
+//    this.parts =  VALID_TIME_PATTERN.exec(time).shift();   
+  }
+
+  seconds = () => this.parts[2]; 
+  minutes = () => this.parts[1];
+  hours = () => this.parts[0];
+}
+
+
+// --------------------------------------------------------------- CLOCK
+class BerlinClock {
+
+  NUMBER_OF_LIGHTS_IN_THE_TOP_ROW = 4;
+  NUMBER_OF_LIGHTS_IN_THE_SECOND_ROW = 4;
+  NUMBER_OF_LIGHTS_IN_THE_THIRD_ROW = 11;
+  NUMBER_OF_LIGHTS_IN_THE_BOTTOM_ROW = 4;
+  RED_LIGHT = 'R';
+  YELLOW_LIGHT = 'Y';
+  OFF_LIGHT = 'O';
+
   constructor(time) {
     this.time = time;
   }
 
-  seconds = () => this.time.split(":")[2];
-  minutes = () => this.time.split(":")[1];
-  hours = () => this.time.split(":")[0];
+  //---SECONDS
+  isEven = (number) => (number % 2 === 0);
+  roundLightOnTop() {
+    return this.isEven(this.time.seconds()) ? this.YELLOW_LIGHT : this.OFF_LIGHT;
+  }
+
+  //--- HOURS
+
+  topRow() {
+    return this.renderRow(
+      this.NUMBER_OF_LIGHTS_IN_THE_TOP_ROW,
+      Math.floor(this.time.hours() / (this.NUMBER_OF_LIGHTS_IN_THE_SECOND_ROW + 1)),
+      this.RED_LIGHT
+    );
+  }
+
+  secondRow() {
+    return this.renderRow(
+      this.NUMBER_OF_LIGHTS_IN_THE_SECOND_ROW,
+      this.time.hours() % (this.NUMBER_OF_LIGHTS_IN_THE_SECOND_ROW + 1),
+      this.RED_LIGHT
+    );
+  }
+
+  // MINUTES
+
+  bottomRow() {
+    return this.renderRow(
+      this.NUMBER_OF_LIGHTS_IN_THE_BOTTOM_ROW,
+      this.time.minutes() % (this.NUMBER_OF_LIGHTS_IN_THE_BOTTOM_ROW + 1),
+      this.YELLOW_LIGHT
+    );
+  };
+
+  thirdRow() {
+    return this.changeQuarterLightsToRed(
+      this.renderRow(
+        this.NUMBER_OF_LIGHTS_IN_THE_THIRD_ROW,
+        Math.floor(this.time.minutes() / (this.NUMBER_OF_LIGHTS_IN_THE_BOTTOM_ROW + 1)),
+        this.YELLOW_LIGHT
+      )
+    );
+  };
+
+
+  changeQuarterLightsToRed = (thirdRowLights) => {
+    const positionOfQuarterRedLights = 3;
+    return thirdRowLights
+      .split('')
+      .map((char, index) => ((index + 1) % positionOfQuarterRedLights === 0 && char === this.YELLOW_LIGHT) ? this.RED_LIGHT : char)
+      .join("");
+  }
+  
+  // TODO extract into class
+  renderRow = (numberOfLights, numberOfLightsOn, color) => {
+    return color.repeat(numberOfLightsOn) + this.OFF_LIGHT.repeat(numberOfLights - numberOfLightsOn);
+  }
+
+
+  render() {
+    return [
+      this.roundLightOnTop(),
+      this.topRow(),
+      this.secondRow(),
+      this.thirdRow(),
+      this.bottomRow()
+    ].join("\n");
+  };
 }
+
+module.exports.berlinClock = berlinClock;
+
+
+
+
+
+
 
 // class BerlinClock {
 //   constructor (time) {
@@ -29,94 +136,3 @@ class ParsedTime {
 //     return this.rendered.toString();
 //   }
 // }
-
-
-function berlinClock(time) {
-
-  if (VALID_TIME_PATTERN.test(time)) {
-    return new BerlinClock(new ParsedTime(time)).render();
-  }
-  return 'error';
-}
-
-
-const RED_LIGHT = 'R';
-const YELLOW_LIGHT = 'Y';
-const OFF_LIGHT = 'O';
-
-const numberOfLightOfTheBottomRow = 4;
-
-
-//TODO: are these two methods something ahve we to move into the berlin clock class?
-const changeQuarterLightsToRed = (thirdRowLights) => {
-  const positionOfQuarterRedLights = 3;
-  return thirdRowLights
-    .split('')
-    .map((char, index) => ((index + 1) % positionOfQuarterRedLights === 0 && char === YELLOW_LIGHT) ? RED_LIGHT : char)
-    .join("");
-}
-
-//TODO: we missed to use this method into the minutes component... let it here and go for further invesdtigation later
-const renderRow = (numberOfLights, numberOfLightsOn, color) => {
-  return color.repeat(numberOfLightsOn) + OFF_LIGHT.repeat(numberOfLights - numberOfLightsOn);
-}
-
-
-
-//class BerlinClock
-class BerlinClock {
-  //---INIT
-  constructor(time) {
-    this.time = time;
-  }
-
-  //---SECONDS
-  isEven = (number) => (number % 2 === 0);
-  roundLightOnTop() {
-    return this.isEven(this.time.seconds()) ? YELLOW_LIGHT : OFF_LIGHT; //TODO: bring these constants inside the class
-  }
-
-  //--- HOURS
-  numberOfLightOfTheSecondRow = 4;
-
-  topRow() {
-    return renderRow(
-      4,
-      Math.floor(this.time.hours() / (this.numberOfLightOfTheSecondRow + 1)),
-      RED_LIGHT
-    );
-  }
-
-  secondRow() {
-    return renderRow(
-      this.numberOfLightOfTheSecondRow,
-      this.time.hours() % (this.numberOfLightOfTheSecondRow + 1),
-      RED_LIGHT
-    );
-  }
-
-  bottomRow() {
-    const numberOfLights = numberOfLightOfTheBottomRow; //TODO: maybe extract it into global to see the composition of the clock
-    const numberOfLightsOn = this.time.minutes() % (numberOfLights + 1);
-    return YELLOW_LIGHT.repeat(numberOfLightsOn) + OFF_LIGHT.repeat(numberOfLights - numberOfLightsOn);
-  };
-
-  thirdRow() {
-    const numberOfLights = 11;
-    const numberOfLightsOn = Math.floor(this.time.minutes() / (numberOfLightOfTheBottomRow + 1));
-    const thirdRowLights = YELLOW_LIGHT.repeat(numberOfLightsOn) + OFF_LIGHT.repeat(numberOfLights - numberOfLightsOn);
-    return changeQuarterLightsToRed(thirdRowLights);
-  };
-
-  render() {
-    return [
-      this.roundLightOnTop(),
-      this.topRow(),
-      this.secondRow(),
-      this.thirdRow(),
-      this.bottomRow()
-    ].join("\n");
-  };
-}
-
-module.exports.berlinClock = berlinClock;
